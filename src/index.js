1c2ede36a277fd2eadea88e41b9355b17ca99740
Juan Robledo <heaxenz@Rikos-Air.lan>
2023-07-09 23:10:30 -0700

    clean

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import fruits from './es-exercies/src/fruits';
+import { choice, remove } from './es-exercies/src/helpers';
+
+let fruit = choice(fruits);
+
+console.log(`I'd like one ${fruit}, please`);
+console.log(`Here you go: ${fruit}`);
+console.log(`Delicious! May I have another?`);
+
+let remaining = remove(fruit, fruits);
+
+console.log(`I'm sorry, we're all out. We have ${remaining.length} left.`);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +27,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
